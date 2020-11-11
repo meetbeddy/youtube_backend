873ff590d6936d1171190390fa28beacbbb71275
meetbeddy <katlybedrick@gmail.com>
2020-11-11 01:09:57 +0100

    changing port

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const ytdl = require("ytdl-core");
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
-app.listen(4000, () => {
-  console.log("Server Works !!! At port 4000");
+app.listen(PORT, () => {
+  console.log(`Server Works !!! At port ${PORT}`);
 });
 
 app.get("/convert", async (req, res) => {
